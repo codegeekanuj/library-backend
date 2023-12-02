09fa33b3a468796b872da469299ff7b10c0cf1a4
anuj tripathi <atripathi1253@gmail.com>
2023-12-02 13:04:52 +0530

    backlib

---
+++ routes/auth.js
@@ -0,0 +1,92 @@
+import express from "express";
+import User from "../models/User.js";
+import bcrypt from "bcrypt";
+
+const router = express.Router();
+
+/* User Registration */
+router.post("/register", async (req, res) => {
+  try {
+    /* Salting and Hashing the Password */
+    const salt = await bcrypt.genSalt(10);
+    const hashedPass = await bcrypt.hash(req.body.password, salt);
+
+    /* Create a new user */
+    const newuser = await new User({
+      userType: req.body.userType,
+      userFullName: req.body.userFullName,
+      admissionId: req.body.admissionId,
+      employeeId: req.body.employeeId,
+      age: req.body.age,
+      dob: req.body.dob,
+      gender: req.body.gender,
+      address: req.body.address,
+      mobileNumber: req.body.mobileNumber,
+      email: req.body.email,
+      password: hashedPass,
+      isAdmin: req.body.isAdmin,
+    });
+
+    
+
+
+    /* Save User and Return */
+    const user = await newuser.save();
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+/* User Login */
+router.post("/signin", async (req, res) => {
+  try {
+    console.log(req.body, "req");
+    const user = req.body.admissionId
+      ? await User.findOne({
+          admissionId: req.body.admissionId,
+        })
+      : await User.findOne({
+          employeeId: req.body.employeeId,
+        });
+
+    console.log(user, "user");
+
+    !user && res.status(404).json("User not found");
+
+    const validPass = await bcrypt.compare(req.body.password, user.password);
+    !validPass && res.status(400).json("Wrong Password");
+
+    res.status(200).json(user);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+// router.get("/registeradmin", async (req, res) => {
+//   try {
+//     const salt = await bcrypt.genSalt(10);
+//     const hashedPassadmin = await bcrypt.hash("atripathi", salt);
+//     const adminuser = await new User({
+//       userType: "librarian",
+//       userFullName: "Anuj Tripathi",
+//       admissionId: "A12345",
+//       employeeId: "A12345",
+//       age: 20,
+//       dob: "31/05/2004",
+//       gender: "mail",
+//       address: "jhansi",
+//       mobileNumber: 83035574074,
+//       email: "atripathi@gmail.com",
+//       password: hashedPassadmin,
+//       isAdmin: true,
+//     });
+//     const adminUser = await adminuser.save();
+//     res.status(200).json(adminUser);
+
+//   } catch (error) {
+//     console.log(err);
+//   }
+// })
+
+export default router;
